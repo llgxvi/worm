bca0067b3a24642d17364a0be71dce07b56c770f
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-02-25 12:59:34 +0800

    Create driver_tpl.c

---
+++ rootkit/driver_tpl.c
@@ -0,0 +1,16 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+module_init(rooty_init);
+module_exit(rooty_exit);
+
+int rooty_init(void) {
+ printk("rooty: module loaded\n");
+ return 0;
+}
+
+void rooty_exit(void) {
+ printk("rooty: module removed\n");
+}
