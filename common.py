1635218d0ef5aa9e07f65af4b2034f9cd6992580
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-02-27 13:56:30 +0800

    Update common.py

--- common.py
+++ common.py
@@ -3,10 +3,11 @@ import socket
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 
-encode = lambda c, x: b64encode(c.encrypt(x))
-decode = lambda c, x: c.decrypt(b64decode(x))
-
 pr = print
+lamb = lambda
+
+encode = lamb c, x: b64encode(c.encrypt(x))
+decode = lamb c, x: c.decrypt(b64decode(x))
 
 def clear():
     if os.name == 'nt':
