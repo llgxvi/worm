d13d8ea3cd272a684a7899c8e3cb06a2cc190113
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-03 15:16:12 +0800

    Update get_sys_call_table.c

--- get_sys_call_table/get_sys_call_table.c
+++ get_sys_call_table/get_sys_call_table.c
@@ -1,26 +1,26 @@
-#include <linux/syscalls.h> // ksys_close
+#include <linux/syscalls.h>
 
-uint64_t **get_table(void) {
+uint64_t **get_table() {
+  // sct: sys_call_table
+
   uint64_t offset = PAGE_OFFSET;
   uint64_t **sct;
 
   while(offset < ULLONG_MAX) {
-    sct = (uint64_t **)offset;
+    sct = (uint64_t**)offset;
 
-    if(sct[__NR_close] == (uint64_t *) ksys_close) {
+    if(sct[__NR_close] == (uint64_t*)ksys_close) {
       printk("🍺 sys_call_table found at address: 0x%p\n", sct);
       return sct;
     }
 
-    offset += sizeof(void *);
+    offset += sizeof(void*);
   }
 
   return NULL;
 }
 
 int f_init(void) {
-  uint64_t **a = get_table();
-  printk("🍺 %lu\n", (uintptr_t)a);
   return 0;
 }
 
