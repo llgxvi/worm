33ac21bedf67b62008db448f93ab96827d94dea4
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-03 20:21:00 +0800

    Update get_sys_call_table.c

--- get_sys_call_table/get_sys_call_table.c
+++ get_sys_call_table/get_sys_call_table.c
@@ -1,18 +1,18 @@
 #include <linux/syscalls.h>
 
-uint64_t **get_table(void) {
+uintptr_t **get_table(void) {
   // sct: sys_call_table
   // ksys_close: kernel version > 5.3
   // PAGE_OFFSET: kernel memory starting point
   // ULLONG_MAX: unsigned long long int max value
 
-  uint64_t **sct;
-  uint64_t offset = PAGE_OFFSET;
+  uintptr_t **sct;
+  uintptr_t offset = PAGE_OFFSET;
 
   while(offset < ULLONG_MAX) {
-    sct = (uint64_t**)offset;
+    sct = (uintptr_t**)offset;
 
-    if(sct[__NR_close] == (uint64_t*)ksys_close) {
+    if(sct[__NR_close] == (uintptr_t*)ksys_close) {
       printk("🍺 sys_call_table found at address: 0x%p\n", sct);
       return sct;
     }
