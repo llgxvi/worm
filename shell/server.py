772dfbabdf3e2e6d0655114cd453c78bfb07d59b
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-04 11:27:03 +0800

    Update server.py

--- shell/server.py
+++ shell/server.py
@@ -2,7 +2,6 @@
 
 import sys
 import socket
-import keyboard 
 from cipher import get_cipher
 from common import pr, cls, _input, Send, Receive
 
@@ -19,13 +18,6 @@ clients = []
 active = False
 
 prompt = ''
-inputt = ''
-
-keyboard.add_hotkey('tab', tab)
-
-def tab():
-  print('\r%s' % inputt)
-  inputt = keyboard.record(until='tab')
 
 def close(sock, client):
   socks.remove(sock)
@@ -102,7 +94,6 @@ while True:
         pr('⚠️', e)
       pr(prompt, end='')
 
-    inputt = keyboard.record(until='tab')
     nc = _input()
 
     if nc == '-1':
