ddfb0cef14c41311f6300d3db15fd4e7d36eb8e4
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-05 18:29:20 +0800

    Update pointer.c

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -10,6 +10,7 @@ int main(void) {
   p2 = &p1;
 
   // value of n
+  // n == *p1 == **p2
   printf("value of n:\n");
   printf("%d\n", n);
   printf("%d\n", *p1);
@@ -17,6 +18,7 @@ int main(void) {
   printf("\n");
 
   // addr of n
+  // &n == p1 == *p2
   printf("addr of n:\n");
   printf("%p\n", &n);
   printf("%p\n", p1);
@@ -24,12 +26,14 @@ int main(void) {
   printf("\n");
 
   // value of p1
+  // p1 == *p2
   printf("value of p1:\n");
   printf("%p\n", p1);
   printf("%p\n", *p2);
   printf("\n");
 
   // addr of p1
+  // &p1 == p2
   printf("addr of p1:\n");
   printf("%p\n", &p1);
   printf("%p\n", p2);
