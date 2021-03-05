3c0072df9f046face0a16ad81fa56189d7d93568
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-05 18:21:03 +0800

    Create pointer.c

---
+++ pointer/pointer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// p2 -> p1 -> n
+int main(void) {
+  int n=123;
+  int *p1;
+  int **p2;
+
+  p1 = &n;
+  p2 = &p1;
+
+  // value of n
+  printf("value of n:\n");
+  printf("%d\n", n);
+  printf("%d\n", *p1);
+  printf("%d\n", **p2);
+  printf("\n");
+
+  // addr of n
+  printf("addr of n:\n");
+  printf("%p\n", &n);
+  printf("%p\n", p1);
+  printf("%p\n", *p2);
+  printf("\n");
+
+  // value of p1
+  printf("value of p1:\n");
+  printf("%p\n", p1);
+  printf("%p\n", *p2);
+  printf("\n");
+
+  // addr of p1
+  printf("addr of p1:\n");
+  printf("%p\n", &p1);
+  printf("%p\n", p2);
+
+  return 0;
+}
