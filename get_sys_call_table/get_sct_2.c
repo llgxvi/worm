ab0eb54cc0078352c3d7b6e65d2153ddac51d7f0
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-03-05 14:10:41 +0800

    Create get_sct_2.c

---
+++ get_sys_call_table/get_sct_2.c
@@ -0,0 +1,30 @@
+#include <linux/syscalls.h>
+
+unsigned long * get_sct(void) {
+  unsigned long *sct;
+  unsigned long i;
+
+  i = (unsigned long)ksys_close;
+  for(; i < ULONG_MAX; i += sizeof(void*)) {
+    sct = (unsigned long *)i;
+
+    if(sct[__NR_close] == i)
+      return sct;
+  }
+
+  return NULL;
+}
+
+int f_init(void) {
+  if(get_sct() == NULL)
+    return -1;
+
+  return 0;
+}
+
+void f_exit(void) {
+}
+
+MODULE_LICENSE("GPL");
+module_init(f_init);
+module_exit(f_exit);
